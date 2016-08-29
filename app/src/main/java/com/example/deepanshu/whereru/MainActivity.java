a59f4a76f367ece7b3812a58873a2e9b64f81aac
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2016-08-29 12:10:15 +0530

    Location Added
    Plz make sure location permission is given in API 23 or higher

--- app/src/main/java/com/example/deepanshu/whereru/MainActivity.java
+++ app/src/main/java/com/example/deepanshu/whereru/MainActivity.java
@@ -1,22 +1,59 @@
 package com.example.deepanshu.whereru;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.*;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-private static final int requestCode=0;
-private TextView userMobileNumberTextBox;
-private String mobileNumber;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    private static final int requestCode = 0;
+    private TextView userMobileNumberTextBox, latitudeTextView, longitudeTextView;
+    private String mobileNumber;
+
+    private double latitude, longitude;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    private android.location.Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mRequestLocationUpdates = false;
+
+    private LocationRequest mLocationRequest;
+
+    private static int UPDATE_INTERVAL = 100;
+    private static int FATEST_INTERVAL = 5000;
+    private static int DISPLACEMENT = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userMobileNumberTextBox=(TextView) findViewById(R.id.user_mobile_no);
+        userMobileNumberTextBox = (TextView) findViewById(R.id.user_mobile_no);
+        latitudeTextView = (TextView) findViewById(R.id.latitude_cordinates);
+        longitudeTextView = (TextView) findViewById(R.id.longitude_cordinates);
 
-        if (savedInstanceState==null) {
-            String mobileNo=MobileNumberPreferences.getMobileNo(this);
+        if (savedInstanceState == null) {
+            String mobileNo = MobileNumberPreferences.getMobileNo(this);
             if (mobileNo == null) {
                 Intent intent = new Intent(this, FirstRun.class);
                 startActivityForResult(intent, requestCode);
@@ -24,11 +61,131 @@ private String mobileNumber;
                 mobileNumber = MobileNumberPreferences.getMobileNo(this);
                 userMobileNumberTextBox.append(mobileNo);
             }
-        }
-        else {
-            mobileNumber=savedInstanceState.getString("MOBILE");
+        } else {
+            mobileNumber = savedInstanceState.getString("MOBILE");
             userMobileNumberTextBox.append(mobileNumber);
         }
+
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+            createLocationRequest();
+        }
+
+        latitudeTextView.setText("Latitude: "+String.valueOf(latitude));
+        longitudeTextView.setText("Longitude: "+String.valueOf(longitude));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkPlayServices();
+        if (mGoogleApiClient.isConnected() && mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //stopLocationUpdates();
+    }
+
+    private void displayLocation() {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                latitude = mLastLocation.getLatitude();
+                longitude = mLastLocation.getLongitude();
+
+                latitudeTextView.setText("Latitude: "+String.valueOf(latitude));
+                longitudeTextView.setText("Longitude: "+String.valueOf(longitude));
+            } else {
+                latitudeTextView.setText("Couldn't get the location. Make sure location is enabled on the device");
+            }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "This device is not supported", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    protected void startLocationUpdates() {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        displayLocation();
+
+        if(mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!", Toast.LENGTH_SHORT).show();
+
+        displayLocation();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: " + connectionResult.getErrorCode());
     }
 
     @Override
@@ -46,4 +203,6 @@ private String mobileNumber;
         super.onSaveInstanceState(outState);
         outState.putString("MOBILE",mobileNumber);
     }
+
+
 }
